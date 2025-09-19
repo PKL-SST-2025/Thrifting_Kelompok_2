0c782870b3ab1cbd7f4337b870d619035d22743c
nayirakhotimah <nayirakhotimah@gmail.com>
2025-09-19 09:18:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./public/index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite//*.js",
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [
+    require('flowbite/plugin'),
+  ],
+}
+
